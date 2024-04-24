5f864f8fa2c17eab618e5e14aa0c8f28439e6e42
agriyantosk <agriyantosk@gmail.com>
2024-04-25 06:15:33 +0700

    chore: add deployment link on README

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## Description
 This web app is a ERC-20 Token Indexer which will query every ERC-20 token that one wallt has aquired. It currently runs on Ethereum Mainnet and built using NextJS, Rainbowkit, Wagmi, and AlchemySDK. It may still contain a lot of bugs such as mobile display since it was for learning purposes.
 
+Live Vercel Deployment: https://erc20-token-indexer-6cgk.vercel.app/
+
 ## Getting Started
 
 First, clone and install the dependencies:
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
     return (
         <>
-            <div className="w-screen h-screen flex flex-col items-center justify-center justify-items-center py-10">
+            <div className="w-screen h-screen flex flex-col gap-3 items-center justify-center justify-items-center py-10">
                 <div>
                     <h1 className="text-4xl font-bold">ERC 20 TOKEN INDEXER</h1>
                 </div>
@@ -73,12 +73,16 @@ export default function Home() {
 
                                     return (
                                         <>
-                                            <div className="flex flex-col items-center gap-3 h-full w-full">
+                                            <div className="flex flex-col items-center gap-3 mt-5 h-full w-full">
                                                 <div
                                                     style={{
                                                         display: "flex",
                                                         gap: 12,
+                                                        borderRadius: 999,
+                                                        border: "1px solid black",
+                                                        padding: 10,
                                                     }}
+                                                    className="hover:bg-gray-200"
                                                 >
                                                     <button
                                                         onClick={openChainModal}
@@ -141,7 +145,7 @@ export default function Home() {
                                                         {account.address}
                                                     </h1>
                                                 </div>
-                                                <div className="flex justify-center items-center h-full w-full">
+                                                <div className="flex justify-center items-center mt-10 h-full w-full">
                                                     <Card
                                                         account={
                                                             account.address
